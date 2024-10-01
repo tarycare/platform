7192a886cb68576141265fbf5ae6f812c4386e02
abusiraj84 <abusiraj84@gmail.com>
2024-10-01 14:01:29 +0300

    s

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Tary Platform
  * Description: A plugin to manage all the Tary's Platform.
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author: Husam Nasrallah
  * Text-Domain: platform
  * GitHub Plugin URI: tarycare/platform
